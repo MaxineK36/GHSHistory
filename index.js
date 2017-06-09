363ab8eea2baa504631f51b9a5e3bee835566929
MaxineK36 <maxine@algernon.com>
2017-06-08 22:38:38 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+
+
+// Create a reference with an initial file path and name
+var storage = firebase.storage();
+var pathReference = storage.ref('images/stars.jpg');
+
+// Create a reference from a Google Cloud Storage URI
+var gsReference = storage.refFromURL('gs://bucket/images/stars.jpg')
+
+// Create a reference from an HTTPS URL
+// Note that in the URL, characters are URL escaped!
+var httpsReference = storage.refFromURL('https://firebasestorage.googleapis.com/b/bucket/o/images%20stars.jpg');
 hideCategories();
-console.log("10:36 version")
+console.log("10:38 version")
 
 var windowHeight = window.innerHeight;
 var windowWidth = window.innerWidth;
